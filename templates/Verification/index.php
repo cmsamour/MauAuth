932b71912a1ce5ac1254bcf03886a67018d703c3
Mauricio Samour <msamour@aloy.bitcloudgroup.com>
2022-03-18 09:58:37 -0600

    Agregado a texto de textbox de codigo unico de 2fa
    In this files are a cakephp files that contain the app to authenticate and use a 2FA

--- templates/Verification/index.php
+++ templates/Verification/index.php
@@ -5,9 +5,9 @@
 	<fieldset>
 		<legend><? = __(singular: 'Proporcione el token generado en su telefono movil') ?> </legend>
 		<div style="text-align: center">
-			<img src="<?= 'data:image/png;base64,'.$imagQr ?>" alt="">
+			<img src="<?= 'data:image/png;base64,'.$imagQr ?>" alt="Imagen QR para escanear">
 		</div>
-		<?= $this->Form->control('token',['required' => true, 'placeholder' => 'Di']) ?>
+		<?= $this->Form->control('token',['required' => true, 'placeholder' => 'Digite el numero proporcionado por su App Authenticator']) ?>
 	</fieldset>
 	<?= $this->Form->submit(__('Verificar')); ?>
 	<?= $this->Form->end() ?>
