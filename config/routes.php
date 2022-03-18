9869bc9fec30d41fdb30506229b78173fa8a1f6b
Mauricio Samour <msamour@aloy.bitcloudgroup.com>
2022-03-18 11:46:01 -0600

    test
    test

--- config/routes.php
+++ config/routes.php
@@ -45,6 +45,17 @@ return static function (RouteBuilder $routes) {
     $routes->setRouteClass(DashedRoute::class);
 
     $routes->scope('/', function (RouteBuilder $builder) {
+
+	// Student Routes
+    $builder->connect('/add-student', ['controller' => 'Students', 'action' => 'addStudent']);
+    $builder->connect('/edit-student/:id', ['controller' => 'Students', 'action' => 'editStudent'], ["pass" => ["id"]]);
+    $builder->connect('/list-students', ['controller' => 'Students', 'action' => 'listStudents']);
+
+    // Ajax Routes
+    $builder->connect('/ajax-add-student', ['controller' => 'Ajax', 'action' => 'ajaxAddStudent']);
+    $builder->connect('/ajax-edit-student', ['controller' => 'Ajax', 'action' => 'ajaxEditStudent']);
+    $builder->connect('/ajax-delete-student', ['controller' => 'Ajax', 'action' => 'ajaxDeleteStudent']);
+
         /*
          * Here, we are connecting '/' (base path) to a controller called 'Pages',
          * its action called 'display', and we pass a param to select the view file
