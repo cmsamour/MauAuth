9a4693d6fb8628f66f7a0d1e68422b9971460746
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-21 10:01:07 -0600

    Ver el usuario en el user view
    App inventario

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -39,6 +39,8 @@ class UsersController extends AppController
         ]);
 
         $this->set(compact('user'));
+        $logeado = $result = $this->Authentication->getResult();
+        $this->set(compact('logeado'));
     }
 
     /**
@@ -106,23 +108,7 @@ class UsersController extends AppController
     }
 
     public function login(){
-    	/*
-	     $this->request->allowMethod(['get', 'post']);
-    	$result = $this->Authentication->getResult();
-    	// regardless of POST or GET, redirect if user is logged in
-    	if ($result->isValid()) {
-    	    // redirect to /articles after login success
-    	    $redirect = $this->request->getQuery('redirect', [
-    	        'controller' => 'Users',
-    	        'action' => 'index',
-    	    ]);
-
-    	    return $this->redirect($redirect);
-    	}
-    	// display error if user submitted and authentication failed
-    	if ($this->request->is('post') && !$result->isValid()) {
-        	$this->Flash->error(__('Invalid username or password'));
-	}*/
+    	
 
 	    $this->request->allowMethod(['get', 'post']);
 	    $result = $this->Authentication->getResult();
