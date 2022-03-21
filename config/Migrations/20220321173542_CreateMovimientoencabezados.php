177d37ea040dd1bb2d7754428a3c71a64f066562
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-21 11:56:22 -0600

    moificamos el migraions de create Movimientoencabezados
    App inventario

--- config/Migrations/20220321173542_CreateMovimientoencabezados.php
+++ config/Migrations/20220321173542_CreateMovimientoencabezados.php
@@ -15,6 +15,58 @@ class CreateMovimientoencabezados extends AbstractMigration
     public function change()
     {
         $table = $this->table('movimientoencabezados');
+
+        $table->addColumn('proveedor_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+	    ]);
+        $table->addColumn('tipomovimiento_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+	    ]);
+        $table->addColumn('turno_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+	    ]);
+        $table->addColumn('fecha', 'date', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('user_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+        ]);
+        $table->addPrimaryKey("id");
+
+        $table->addIndex([
+
+            'fecha',
+
+            ], [
+            'name' => 'FECHA',
+            
+            ]);
+
+
+        //Llaves foraneas van aca.
+        $table->addForeignKey('user_id', 'users', ['id'],
+                            ['constraint'=>'fk_user_movimientoencabezado']);
+        $table->addForeignKey('turno_id', 'turnos', ['id'],
+                            ['constraint'=>'fk_turno_movimientoencabezado']); 
+        $table->addForeignKey('tipomovimiento_id', 'tipomovimientos', ['id'],
+                            ['constraint'=>'fk_tipomovimiento_movimientoencabezado']);
+        $table->addForeignKey('proveedor_id', 'proveedors', ['id'],
+                            ['constraint'=>'fk_proveedor_movimientoencabezado']); 
+
+
         $table->create();
     }
 }
