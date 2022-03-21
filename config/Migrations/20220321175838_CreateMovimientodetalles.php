35ae330e46b78f63290683db5a9b02eea9fc9d85
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-21 12:17:21 -0600

    moificamos el migraions de create Movimientodetalles
    App inventario

--- config/Migrations/20220321175838_CreateMovimientodetalles.php
+++ config/Migrations/20220321175838_CreateMovimientodetalles.php
@@ -15,6 +15,79 @@ class CreateMovimientodetalles extends AbstractMigration
     public function change()
     {
         $table = $this->table('movimientodetalles');
+
+        $table->addColumn('movimientoencabezado_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+	    ]);
+        $table->addColumn('producto_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+	    ]);
+        $table->addColumn('pruto', 'decimal', [
+            'default' => null,
+            'null' => false,
+            'precision' => 5,
+            'scale' => 2,
+        ]);
+        $table->addColumn('tara', 'decimal', [
+            'default' => null,
+            'null' => false,
+            'precision' => 5,
+            'scale' => 2,
+        ]);
+        $table->addColumn('pneto', 'decimal', [
+            'default' => null,
+            'null' => false,
+            'precision' => 5,
+            'scale' => 2,
+        ]);
+        $table->addColumn('created', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('user_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+        ]);
+
+        $table->addPrimaryKey("id");
+
+        $table->addIndex([
+
+            'pneto',
+
+            ], [
+            'name' => 'PNETO',
+            
+            ]);
+            $table->addIndex([
+
+                'pbruto',
+    
+                ], [
+                'name' => 'PBRUTO',
+                
+            ]);
+            $table->addIndex([
+
+                'tara',
+        
+                ], [
+                    'name' => 'TARA',
+                    
+            ]);
+        $table->addForeignKey('user_id', 'users', ['id'],
+                            ['constraint'=>'fk_user_movimientodetalle']);
+        $table->addForeignKey('producto_id', 'prodcutos', ['id'],
+                            ['constraint'=>'fk_producto_movimientodetalle']);
+                            $table->addForeignKey('movimientoencabezado_id', 'movimientoencabezados', ['id'],
+                            ['constraint'=>'fk_movimientoencabezado_movimientodetalle']);            
+
         $table->create();
     }
 }
