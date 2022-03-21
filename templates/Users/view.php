c9ddd1f1a1a3328704bb5cd64346fd39b0cd289e
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-21 10:08:19 -0600

    Ver el usuario en el user view
    App inventario

--- templates/Users/view.php
+++ templates/Users/view.php
@@ -21,7 +21,7 @@
                 <tr>
                     <th><?= __('Email') ?></th>
                     <td><?= h($user->email) ?>
-                    </td><?= h($logeadoa['email']) ?></td>
+                    </td><?= h($logeado['email']) ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Phone Number') ?></th>
