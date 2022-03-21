9c32c53e095c169aa89521d31a349dc5a5ac3ddb
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-21 11:27:20 -0600

    moificamos el migraions de create tipomovimientos
    App inventario

--- config/Migrations/20220321172246_CreateTipomovimientos.php
+++ config/Migrations/20220321172246_CreateTipomovimientos.php
@@ -15,6 +15,40 @@ class CreateTipomovimientos extends AbstractMigration
     public function change()
     {
         $table = $this->table('tipomovimientos');
+
+        $table->addColumn('nombre', 'string', [
+            'limit' => 120,
+            'null' => false,
+	    ]);
+        $table->addColumn('espositivo', 'boolean', [
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('user_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+	    ]);
+	    $table->addColumn('activo', 'boolean', [
+            'null' => false,
+        ]);
+
+
+        //$table->addPrimaryKey("id");
+        $table->addIndex([
+
+            'nombre',
+
+            ], [
+            'name' => 'UNIQUE_NOMBRE',
+            'unique' => true,
+            ]);
         $table->create();
     }
 }
