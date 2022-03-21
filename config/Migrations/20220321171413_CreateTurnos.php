f6a385eef5200e586c791f2423022c574b61e5de
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-21 11:15:10 -0600

    moificamos el migraions de create turnos
    App inventario

--- config/Migrations/20220321171413_CreateTurnos.php
+++ config/Migrations/20220321171413_CreateTurnos.php
@@ -15,6 +15,46 @@ class CreateTurnos extends AbstractMigration
     public function change()
     {
         $table = $this->table('turnos');
+
+        $table->addColumn('nombre', 'string', [
+            'limit' => 120,
+            'null' => false,
+	    ]);
+        $table->addColumn('horainicial', 'timestamp', [
+        'default' => 'CURRENT_TIMESTAMP',
+        'null' => false,
+        ]);
+        $table->addColumn('horafinal', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+            ]);
+        $table->addColumn('created', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'timestamp', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false,
+        ]);
+        $table->addColumn('user_id', 'integer', [
+            'limit' => 5,
+            'null' => false,
+	    ]);
+	    $table->addColumn('activo', 'boolean', [
+            'null' => false,
+        ]);
+
+
+        //$table->addPrimaryKey("id");
+        $table->addIndex([
+
+            'nombre',
+
+            ], [
+            'name' => 'UNIQUE_NOMBRE',
+            'unique' => true,
+            ]);
+
         $table->create();
     }
 }
