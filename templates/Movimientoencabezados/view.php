24c0bf35623c01e4bb0a1d3d5e455d16fbf6faff
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-22 11:16:24 -0600

    moificamos el view de movimientosencabezados
    App inventario

--- templates/Movimientoencabezados/view.php
+++ templates/Movimientoencabezados/view.php
@@ -88,7 +88,7 @@
                     </table>
                 </div>
                 <?php endif; ?>
-                <?= $this->Html->link(__('NUEVO LISTA DE VIÑETA'), ['controller' => 'Movimientodetalles',['action' => 'add'], ['class' => 'button float-right']) ?>
+                <?= $this->Html->link(__('New Movimientodetalle'), ['action' => 'add'], ['class' => 'button float-right']) ?>
             </div>
         </div>
     </div>
