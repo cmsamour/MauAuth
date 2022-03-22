6cc9550b4352fcce3384885539d3297bba7258f8
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-21 18:15:23 -0600

    moificamos el view de movimientosencabezados
    App inventario

--- templates/Movimientoencabezados/index.php
+++ templates/Movimientoencabezados/index.php
@@ -26,7 +26,7 @@
                 <?php foreach ($movimientoencabezados as $movimientoencabezado): ?>
                 <tr>
                     <td><?= $this->Number->format($movimientoencabezado->id) ?></td>
-                    <td><?= $movimientoencabezado->has('proveedor') ? $this->Html->link($movimientoencabezado->proveedor->id, ['controller' => 'Proveedors', 'action' => 'view', $movimientoencabezado->proveedor->id]) : '' ?></td>
+                    <td><?= $movimientoencabezado->has('proveedor') ? $this->Html->link($movimientoencabezado->proveedor->nombre, ['controller' => 'Proveedors', 'action' => 'view', $movimientoencabezado->proveedor->id]) : '' ?></td>
                     <td><?= $movimientoencabezado->has('tipomovimiento') ? $this->Html->link($movimientoencabezado->tipomovimiento->nombre, ['controller' => 'Tipomovimientos', 'action' => 'view', $movimientoencabezado->tipomovimiento->id]) : '' ?></td>
                     <td><?= $movimientoencabezado->has('turno') ? $this->Html->link($movimientoencabezado->turno->nombre, ['controller' => 'Turnos', 'action' => 'view', $movimientoencabezado->turno->id]) : '' ?></td>
                     <td><?= h($movimientoencabezado->fecha) ?></td>
@@ -34,9 +34,9 @@
                     <td><?= h($movimientoencabezado->modified) ?></td>
                     <td><?= $movimientoencabezado->has('user') ? $this->Html->link($movimientoencabezado->user->email, ['controller' => 'Users', 'action' => 'view', $movimientoencabezado->user->id]) : '' ?></td>
                     <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $movimientoencabezado->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $movimientoencabezado->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $movimientoencabezado->id], ['confirm' => __('Are you sure you want to delete # {0}?', $movimientoencabezado->id)]) ?>
+                        <?= $this->Html->link(__('VER'), ['action' => 'view', $movimientoencabezado->id]) ?>
+                        <?= $this->Html->link(__('MODIFICAR'), ['action' => 'edit', $movimientoencabezado->id]) ?>
+                        <?= $this->Form->postLink(__('ELIMINAR'), ['action' => 'delete', $movimientoencabezado->id], ['confirm' => __('Are you sure you want to delete # {0}?', $movimientoencabezado->id)]) ?>
                     </td>
                 </tr>
                 <?php endforeach; ?>
@@ -45,11 +45,11 @@
     </div>
     <div class="paginator">
         <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
+            <?= $this->Paginator->first('<< ' . __('primero')) ?>
+            <?= $this->Paginator->prev('< ' . __('anterior')) ?>
             <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
+            <?= $this->Paginator->next(__('siguiente') . ' >') ?>
+            <?= $this->Paginator->last(__('ultimo') . ' >>') ?>
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
