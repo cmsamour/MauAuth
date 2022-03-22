42e92f5eb1a685d3e39e34440a1a70476b8a1339
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-22 14:30:54 -0600

    moificamos el view de movimientosencabezados view con detalle
    App inventario

--- templates/Movimientodetalles/add.php
+++ templates/Movimientodetalles/add.php
@@ -23,7 +23,7 @@
                 <?php
                     //echo $this->Form->control('movimientoencabezado_id', ['options' => $movimientoencabezados]);
                     echo $this->Form->text('movimientoencabezado_id',
-                    ['value' => $movimientoencabezados->id]
+                    ['value' => $movimientoencabezados->id, 'hidden' => 'true']
                 );
                     echo $this->Form->control('producto_id', ['options' => $productos]);
                     echo $this->Form->control('pbruto');
