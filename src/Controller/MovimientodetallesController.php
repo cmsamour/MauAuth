6fb07aa3bbf15e5f058c3b4362018f7d72f00ea0
Mauricio Samour <msamour@Mauricios-MacBook-Pro.local>
2022-03-22 12:11:28 -0600

    moificamos el view de movimientosencabezados view con detalle
    App inventario

--- src/Controller/MovimientodetallesController.php
+++ src/Controller/MovimientodetallesController.php
@@ -62,8 +62,8 @@ class MovimientodetallesController extends AppController
             }
             
             //$movimientoencabezados = $this->Movimientodetalles->Movimientoencabezados->find('list', ['limit' => 200])->all();
-            //$movimientoencabezados  = $this->Movimientoencabezados->get($id);
-            print_r($movimientodetalle->movimientoencabezado_id);
+            $movimientoencabezados  = $this->Movimientoencabezados->get($id);
+            //print_r($movimientodetalle->movimientoencabezado_id);
             //$movimientodetalle->movimientoencabezado_id  = $id;
             $productos = $this->Movimientodetalles->Productos->find('list', ['limit' => 200])->all();
             $users = $this->Movimientodetalles->Users->find('list', ['limit' => 200])->all();
